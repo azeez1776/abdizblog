d8715026bac2af75c3485b05913e8c68483f4dc9
azeez1776 <aboud3am81@gmail.com>
2024-05-08 21:54:07 +0300

    :construction: minor change

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Image src={'/static/images/logo.png'} alt="Logo" width="64" height="64" />
+              <Image src={'/static/images/logo.png'} alt="Logo" width="34" height="34" />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
