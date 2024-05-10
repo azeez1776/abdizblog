65c8615bbcd31c7d739e17589d0603c383fc78fd
azeez1776 <aboud3am81@gmail.com>
2024-05-10 13:47:47 +0300

    :art: added shaky bird

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+import 'css/header.css'
+
 import Image from 'next/image'
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
@@ -13,7 +15,13 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Image src={'/static/images/logo.png'} alt="Logo" width="34" height="34" />
+              <Image
+                src={'/static/images/logo.png'}
+                className="hover-element"
+                alt="Logo"
+                width="34"
+                height="34"
+              />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
