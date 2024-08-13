a0718ec74ac72638ecf27b2d3cfd2debc16e3ee0
azeez1776 <aboud3am81@gmail.com>
2024-08-13 15:21:12 +0300

    added my resume

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -8,6 +8,7 @@ import {
   Mastodon,
   Threads,
   Instagram,
+  Resume,
 } from './icons'
 
 const components = {
@@ -20,6 +21,7 @@ const components = {
   mastodon: Mastodon,
   threads: Threads,
   instagram: Instagram,
+  resume: Resume,
 }
 
 type SocialIconProps = {
--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -27,12 +27,25 @@ export default function AuthorLayout({ children, content }: Props) {
             <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
+            <div className="flex items-center gap-x-1 pt-2 text-gray-500 dark:text-gray-400">
+              <SocialIcon
+                kind="resume"
+                href={
+                  'https://drive.google.com/file/d/1FpI5_j8qOa7tEL1P2u24czEjMOkAXliY/view?usp=sharing'
+                }
+              />
+
+              <a href="https://drive.google.com/file/d/1FpI5_j8qOa7tEL1P2u24czEjMOkAXliY/view?usp=sharing">
+                Resume/CV
+              </a>
+            </div>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
               <SocialIcon kind="twitter" href={twitter} />
             </div>
+            <div></div>
           </div>
           <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
             {children}
